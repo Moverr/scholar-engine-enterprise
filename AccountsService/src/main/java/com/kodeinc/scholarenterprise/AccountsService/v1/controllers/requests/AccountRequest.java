ecc2ab2ce70ba5472e89f935d12390786d0eeac6
Muyinda ROgers <moverr@gmail.com>
2021-05-31 14:16:36 +0300

    :Debuggging

---
+++ AccountsService/src/main/java/com/kodeinc/scholarenterprise/AccountsService/v1/controllers/requests/AccountRequest.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kodeinc.scholarenterprise.AccountsService.v1.controllers.requests;
+
+/**
+ *
+ * @author muyin
+ */
+public class AccountRequest {
+    
+}
