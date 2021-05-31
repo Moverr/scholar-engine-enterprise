8e9a8be9618f43461e58a31390046eb3ea72d7a5
Muyinda ROgers <moverr@gmail.com>
2021-05-31 14:13:13 +0300

    :Debuggging

---
+++ AccountsService/src/main/java/com/kodeinc/scholarenterprise/AccountsService/v1/controllers/AccountController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kodeinc.scholarenterprise.AccountsService.v1.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author mover
+ * 5/31/2021
+ */
+@RestController
+@RequestMapping("v1/account")
+public class AccountController {
+    
+    //todo: Create Account
+    
+    //todo: Login
+     
+    
+    //todo : ForgotPassword
+    
+    //todo: Reset Password 
+    
+    
+    
+}
