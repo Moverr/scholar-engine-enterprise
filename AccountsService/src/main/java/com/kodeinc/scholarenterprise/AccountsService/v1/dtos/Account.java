e3667b12877daa8737d7ce04040601b3266b1084
Muyinda ROgers <moverr@gmail.com>
2021-06-03 22:19:16 +0300

    :Debugggiing

--- AccountsService/src/main/java/com/kodeinc/scholarenterprise/AccountsService/v1/dtos/Account.java
+++ AccountsService/src/main/java/com/kodeinc/scholarenterprise/AccountsService/v1/dtos/Account.java
@@ -5,6 +5,8 @@
  */
 package com.kodeinc.scholarenterprise.AccountsService.v1.dtos;
 
+import com.kodeinc.scholarenterprise.AccountsService.v1.helpers.AccountStatus;
+import java.lang.annotation.Documented;
 import java.sql.Timestamp;
 import javax.validation.constraints.NotBlank;
 import org.springframework.data.annotation.Id;
@@ -13,6 +15,7 @@ import org.springframework.data.annotation.Id;
  *
  * @author muyin
  */
+ 
 public class Account {
 
     @Id
@@ -26,8 +29,80 @@ public class Account {
     public String email;
     
     public long author_id;
+    
     public Timestamp date_created;
-    public Enum status;
+    public AccountStatus status;
+    public Timestamp date_updated;
+
+    public Account() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(long author_id) {
+        this.author_id = author_id;
+    }
+
+    public Timestamp getDate_created() {
+        return date_created;
+    }
+
+    public void setDate_created(Timestamp date_created) {
+        this.date_created = date_created;
+    }
+
+    public AccountStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(AccountStatus status) {
+        this.status = status;
+    }
+
+    public Timestamp getDate_updated() {
+        return date_updated;
+    }
+
+    public void setDate_updated(Timestamp date_updated) {
+        this.date_updated = date_updated;
+    }
+    
+    
+    
     
     
     
