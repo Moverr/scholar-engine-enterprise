09b7d567e24ded76cdb9c9c5d52d39c07fcb2df0
Muyinda ROgers <moverr@gmail.com>
2021-06-06 12:06:37 +0300

    :DEbuggging AccountResponse

--- AccountsService/src/main/java/com/kodeinc/scholarenterprise/AccountsService/v1/controllers/responses/AccountResponse.java
+++ AccountsService/src/main/java/com/kodeinc/scholarenterprise/AccountsService/v1/controllers/responses/AccountResponse.java
@@ -6,9 +6,7 @@
 package com.kodeinc.scholarenterprise.AccountsService.v1.controllers.responses;
 
 import com.kodeinc.scholarenterprise.AccountsService.v1.helpers.AccountStatus;
-import java.sql.Timestamp;
-import javax.validation.constraints.NotBlank;
-import org.springframework.data.annotation.Id;
+import java.util.Objects;
 
 /**
  *
@@ -22,5 +20,103 @@ public class AccountResponse {
     public String author; 
     public Long date_created;
     public AccountStatus status;
+
+    public AccountResponse() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Long getDate_created() {
+        return date_created;
+    }
+
+    public void setDate_created(Long date_created) {
+        this.date_created = date_created;
+    }
+
+    public AccountStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(AccountStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.id);
+        hash = 37 * hash + Objects.hashCode(this.firstName);
+        hash = 37 * hash + Objects.hashCode(this.lastName);
+        hash = 37 * hash + Objects.hashCode(this.email);
+        hash = 37 * hash + Objects.hashCode(this.author);
+        hash = 37 * hash + Objects.hashCode(this.date_created);
+        hash = 37 * hash + Objects.hashCode(this.status);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final AccountResponse other = (AccountResponse) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.firstName, other.firstName)) {
+            return false;
+        }
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        if (!Objects.equals(this.author, other.author)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
 }
